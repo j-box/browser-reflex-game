571993d275ce1fddff2fe342b9f560011a5b4488
Jesse Luna-Mendoza <jesselm@Jesses-Windows-PC.local>
2022-02-15 13:56:15 -0800

    Complete version 3 (with timer and adjustable turns)

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const board = document.querySelector('#gameBoard');
 const piece = document.querySelector('.gamePiece');
+const page = document.querySelector('#startPage');
+const startButt = document.querySelector('#startGame');
 const pieHeight = piece.offsetHeight;
 const pieWidth = piece.offsetWidth;
 const winHeight = window.screen.height;
@@ -8,37 +10,38 @@ const winWidth = window.screen.width;
 let seconds = 0;
 let tens = 0;
 
-function startTimer () {
-    tens++;
-    
-    if (tens > 99) {
-      seconds++;
-      tens = 0;
-    }
-  
+function startTimer() {
+  tens++;
+
+  if (tens > 99) {
+    seconds++;
+    tens = 0;
   }
 
-let interval = setInterval(startTimer, 10);
+}
 
-let gameLength = 12;
+// let gameLength = 3;
+let gameLength = parseInt(window.localStorage.getItem('clicks'));
 let counter = 0;
 
+let interval = setInterval(startTimer, 10);
 piece.style.top = Math.abs(((Math.random() * winHeight) - pieHeight)).toString() + 'px';
 piece.style.left = Math.abs(((Math.random() * winWidth) - pieWidth)).toString() + 'px';
 
 piece.addEventListener('click', () => {
-    const vert = Math.abs(((Math.random() * winHeight) - pieHeight)).toString();
-    const hor = Math.abs(((Math.random() * winWidth) - pieWidth)).toString();
-    if (piece.click) {
-        piece.style.top = vert + 'px';
-        piece.style.left = hor + 'px';
-        counter++;
-        if (counter === gameLength) {
-            alert(`clicked ${gameLength} times in ${seconds}.${tens<=9?'0'+tens:tens} seconds`);
-            counter = 0;
-            window.location.replace('start.html');
-            clearInterval(interval);
-        }
+  const vert = Math.abs(((Math.random() * winHeight) - pieHeight)).toString();
+  const hor = Math.abs(((Math.random() * winWidth) - pieWidth)).toString();
+  if (piece.click) {
+    piece.style.top = vert + 'px';
+    piece.style.left = hor + 'px';
+    counter++;
+    if (counter === gameLength) {
+      counter = 0;
+      clearInterval(interval);
+      alert(`clicked ${gameLength} times in ${seconds}.${tens <= 9 ? '0' + tens : tens} seconds`);
+      window.localStorage.setItem('clicks', '3');
+      window.location.replace('start.html');
     }
+  }
 })
 
--- start.js
+++ start.js
@@ -1,5 +1,25 @@
 const page = document.querySelector('#startPage');
 const startButt = document.querySelector('#startGame');
+const clicksButt = document.querySelector('.numClicks');
+// let clicksNum = clicksButt.value;
+let clicksNum = 3
+
+// window.onload = () => {
+//     let clicksNum = 3;
+//     clicksButt.addEventListener('change', () => {
+//         clicksNum = clicksButt.value;
+//         window.localStorage.setItem('clicks', clicksNum);
+//     })
+    
+//     startButt.addEventListener('click', () => {
+//         window.location.replace('game.html');
+//     })
+// }
+
+clicksButt.addEventListener('change', () => {
+    clicksNum = clicksButt.value;
+    window.localStorage.setItem('clicks', clicksNum);
+})
 
 startButt.addEventListener('click', () => {
     window.location.replace('game.html');
